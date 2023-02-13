8d8c7c9762331ac2d8235b1768cf99ec34d2c8eb
Nicholas <nicholas.an99@gmail.com>
2023-02-13 17:24:27 -0300

    Removing useless content

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,13 +8,7 @@ const prisma = new PrismaClient()
 app.register(cors)
 
 app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber'
-      }
-    }
-  })
+  const habits = await prisma.habit.findMany()
 
   return habits
 })
