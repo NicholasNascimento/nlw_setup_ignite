661f702558b2a023baeb1f3f3d83693031d05994
Nicholas <nicholas.an99@gmail.com>
2023-02-21 22:26:47 -0300

    Adding radix

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Plus } from 'phosphor-react'
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImage from '../assets/logo.svg'
 
@@ -7,13 +8,23 @@ export function Header() {
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={logoImage} alt="Logo" />
 
-      <button
-        type='button'
-        className='border border-violet-500 font-semibold text-white rounded-lg px-6 py-4 flex items-center hover:border-violet-300'
-      >
-        <Plus size={20} className='text-violet-500' />
-        Novo hábito
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger
+          type='button'
+          className='border border-violet-500 font-semibold text-white rounded-lg px-6 py-4 flex items-center hover:border-violet-300'
+        >
+          <Plus size={20} className='text-violet-500' />
+          Novo hábito
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0' />
+
+          <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2'>
+            Conteúdo do modal
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
