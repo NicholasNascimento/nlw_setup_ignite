f2082e4899815a6d3d16d532f966740499e33a70
Nicholas <nicholas.an99@gmail.com>
2023-02-22 00:12:41 -0300

    Updating content

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Plus, X } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImage from '../assets/logo.svg'
+import { NewHabitForm } from './NewHabitForm';
 
 export function Header() {
   return (
@@ -25,7 +26,11 @@ export function Header() {
               <X size={24} aria-label="Fechar" />
             </Dialog.Close>
 
-            Conteúdo do modal
+            <Dialog.Title className='text-white text-3xl leading-ti font-extrabold'>
+              Criar hábito
+            </Dialog.Title>
+
+            <NewHabitForm />
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
