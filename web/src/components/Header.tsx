7b63e6626cde1dd61c149325d3585fa85092ce88
Nicholas <nicholas.an99@gmail.com>
2023-03-02 11:59:19 -0300

    Adding transitions

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -12,7 +12,7 @@ export function Header() {
       <Dialog.Root>
         <Dialog.Trigger
           type='button'
-          className='border border-violet-500 font-semibold text-white rounded-lg px-6 py-4 flex items-center hover:border-violet-300'
+          className='border border-violet-500 font-semibold text-white rounded-lg px-6 py-4 flex items-center hover:border-violet-300 transition-colors'
         >
           <Plus size={20} className='text-violet-500' />
           Novo hábito
