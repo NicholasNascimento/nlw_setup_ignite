b978a3ca1b432f334e730c73ccb0c5f5881f5863
Nicholas <nicholas.an99@gmail.com>
2023-03-09 18:21:54 -0300

    Updating content

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -65,7 +65,15 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
             disabled={isDateInPast}
             className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
           >
-            
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+              {habit.title}
+            </span>
           </Checkbox.Root>
         )
       })}
